76e6b48578328c8086d7acc21d762e0b684a0402
Miguel Fonseca <miguel.cnf@gmail.com>
2018-04-10 00:45:21 +0100

    Add hammerspoon init config

---
+++ .hammerspoon-init.lua
@@ -0,0 +1,25 @@
+-- General
+hs.console.clearConsole()
+
+-- Audio Device Watching
+logger = hs.logger.new("audioDevice", "debug")
+
+audioDeviceWatcher = nil
+currentDevice = hs.audiodevice.defaultOutputDevice():name()
+
+function audioDeviceChangedCallback()
+    newDevice = hs.audiodevice.defaultOutputDevice():name()
+
+    spotifyIsPlaying = hs.spotify.isPlaying()
+
+    if currentDevice ~= "miguel-bose" and newDevice == "Built-in Output" and spotifyIsPlaying == true then
+        hs.spotify.pause();
+        hs.notify.new({title="Hammerspoon", informativeText="Paused Spotify"}):send()
+    end
+
+    currentDevice = newDevice
+end
+
+audioDeviceWatcher = hs.audiodevice.watcher
+audioDeviceWatcher.setCallback(audioDeviceChangedCallback)
+audioDeviceWatcher:start()
